75eb3cd3d31d4751b28a8f19cc1e74118ef81049
Sean Millichamp <sean.millichamp@secure-24.com>
2020-11-09 21:39:23 +0000

    Add support for PE package inventory facts
    PuppetDB returns those from an endpoint independent from the
    rest of the inventory. This extracts them from that endpoint,
    if available, and restructures them to match the way facter
    returns them so they can be fed in as source facts.

--- lib/octocatalog-diff/facts/puppetdb.rb
+++ lib/octocatalog-diff/facts/puppetdb.rb
@@ -36,6 +36,7 @@ module OctocatalogDiff
         exception_class = nil
         exception_message = nil
         obj_to_return = nil
+        packages = nil
         (retries + 1).times do
           begin
             result = puppetdb.get(uri)
@@ -61,8 +62,46 @@ module OctocatalogDiff
             exception_message = "Fact retrieval failed for node #{node} from PuppetDB (#{exc.message})"
           end
         end
-        return obj_to_return unless obj_to_return.nil?
-        raise exception_class, exception_message
+
+        raise exception_class, exception_message if obj_to_return.nil?
+
+        return obj_to_return if puppetdb_api_version < 4
+
+        (retries + 1).times do
+          begin
+            result = puppetdb.get("/pdb/query/v4/package-inventory/#{node}")
+            packages = {}
+            result.each do |pkg|
+              key = "#{pkg['package_name']}+#{pkg['provider']}"
+              # Need to handle the situation where a package has multiple versions installed.
+              # The _puppet_inventory_1 hash lists them separated by "; ".
+              if packages.key?(key)
+                packages[key]['version'] += "; #{pkg['version']}"
+              else
+                packages[key] = pkg
+              end
+            end
+            break
+          rescue OctocatalogDiff::Errors::PuppetDBConnectionError => exc
+            exception_class = OctocatalogDiff::Errors::FactSourceError
+            exception_message = "Fact packages retrieval failed (#{exc.class}) (#{exc.message})"
+          rescue OctocatalogDiff::Errors::PuppetDBNodeNotFoundError
+            packages = {}
+          rescue OctocatalogDiff::Errors::PuppetDBGenericError => exc
+            exception_class = OctocatalogDiff::Errors::FactRetrievalError
+            exception_message = "Fact packages retrieval failed for node #{node} from PuppetDB (#{exc.message})"
+          end
+        end
+
+        raise exception_class, exception_message if packages.nil?
+
+        unless packages.empty?
+          obj_to_return['values']['_puppet_inventory_1'] = {
+            'packages' => packages.values.map { |pkg| [pkg['package_name'], pkg['version'], pkg['provider']] }
+          }
+        end
+
+        obj_to_return
       end
     end
   end
